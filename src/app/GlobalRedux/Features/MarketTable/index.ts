51b55f547bdc214a3e0b47e7bd24abd73ce9b4da
Kyle Starrett <akstarre@gmail.com>
2023-09-19 17:56:07 -0600

    trying to optimize ts for vercel. Stuck on coin in page.tsx and ChartSelector's index.tsx. Will make progress tomorrow.

--- src/app/GlobalRedux/Features/MarketTable/index.ts
+++ src/app/GlobalRedux/Features/MarketTable/index.ts
@@ -1,36 +1,34 @@
 `use client`;
 
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { Coin } from "../../../../../interfaces";
 
-
-
-export const fetchCoins = createAsyncThunk('coins/getCoins', 
-    async (currency:string, thunkApi) => {
-    const COIN_URL= `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en`
-    const response = await fetch(COIN_URL)
+export const fetchCoins = createAsyncThunk(
+  "coins/getCoins",
+  async (currency: string, thunkApi) => {
+    const COIN_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en`;
+    const response = await fetch(COIN_URL);
     const data = await response.json();
-    console.log(data)
-    return data
-})
+    console.log(data);
+    return data;
+  }
+);
 
-const initialState = {
-    coins: [],
-    loading: false,
-    error: "",
-}
+const initialState: { coins: Coin[]; loading: boolean; error: Error | null } = {
+  coins: [],
+  loading: false,
+  error: null,
+};
 
 const marketTableSlice = createSlice({
-    name: "coins",
-    initialState,
-    reducers: {
+  name: "coins",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCoins.fulfilled, (state, action) => {
+      state.coins = action.payload;
+    });
+  },
+});
 
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchCoins.fulfilled, (state, action)=> {
-            state.coins = action.payload
-        })
-    }
-})
-
-
-export default marketTableSlice.reducer
+export default marketTableSlice.reducer;
--- src/components/MarketTable/index.tsx
+++ src/components/MarketTable/index.tsx
@@ -1,46 +1,12 @@
 "use client";
 
-interface Coin {
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: string;
-  atl: number;
-  atl_change_percentage: number;
-  atl_date: string;
-  circulating_supply: number;
-  current_price: number;
-  fully_diluted_valuation: number;
-  high_24h: number;
-  id: string;
-  image: string;
-  last_updated: string;
-  low_24h: number;
-  market_cap: number;
-  market_cap_change_24h: number;
-  market_cap_change_percentage_24h: number;
-  market_cap_rank: number;
-  max_supply: number;
-  name: string;
-  price_change_24h: number;
-  price_change_percentage_1h_in_currency: number;
-  price_change_percentage_7d_in_currency: number;
-  price_change_percentage_24h: number;
-  price_change_percentage_24h_in_currency: number;
-  roi: null | number;
-  sparkline_in_7d: {
-    price: number[];
-  };
-  symbol: string;
-  total_supply: number;
-  total_volume: number;
-}
-
 interface MarketTableProps {
   coins?: Coin[];
   loading: boolean;
   error: string;
 }
 
+import { Coin } from "../../../interfaces";
 import { MarketListItem } from "../MarketListItem";
 
 const tableCategories = [
@@ -78,7 +44,7 @@ const MarketTable: React.FC<MarketTableProps> = ({ coins, loading, error }) => {
           </thead>
           <tbody>
             {coins?.map((coin, index) => (
-              <MarketListItem key={coin.id} coin={coin} index={index} />
+              <MarketListItem key={coin.symbol} coin={coin} index={index} />
             ))}
           </tbody>
         </table>
