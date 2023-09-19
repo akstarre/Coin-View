ae6c9ef137c94aa3fceb71bed432f376427aa7ac
Kyle Starrett <akstarre@gmail.com>
2023-09-19 14:12:46 -0600

    added hexadecimal colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "purple-highlight": "#6161D6",
+        "purple-border": "#7878FA",
+        "dark-purple": "#1E1932",
+        "black-purple": "#13121A",
+        "grey-purple-1": "#191925",
+        "grey-purple-2": "#232336",
+        "grey-text": "#D1D1D1",
+        
+        "green-change": "#01F1E3",
+        "red-change": "#FE2264",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
