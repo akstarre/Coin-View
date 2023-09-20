8e76ab29c135d84c42f446fbfc39742f524c2152
Kyle Starrett <akstarre@gmail.com>
2023-09-20 10:41:27 -0600

    fixed dropdownRef potentially being null in Dropdown component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,56 +1,56 @@
-"use client"
+"use client";
 
-import {useState, useEffect, useRef} from "react"
-import { CurrencyDropdownList} from "../CurrencyDropdownList/index"
+import { useState, useEffect, useRef } from "react";
+import { CurrencyDropdownList } from "../CurrencyDropdownList/index";
 
 type DropdownProps = {
   handleSelection: (currency: string) => void;
   currentCurrency: string;
-}
+};
 
-const currencyList = ["usd", "eur", "jpy"]
+const currencyList = ["usd", "eur", "jpy"];
 
-export const Dropdown = ({ handleSelection, currentCurrency }: DropdownProps) => {
-    const [dropDownOpen, setDropDownOpen] = useState(false);
-  
-    const closeDropDown = () => {
-      setDropDownOpen(false);
-    };
-  
-    const updateDropDown = () => {
-      setDropDownOpen(!dropDownOpen);
-    };
-  
-    const handleDropSelection = (selection:string) => {
-      handleSelection(selection)
-      closeDropDown();
-    };
-  
-    const dropdownRef = useRef(null);
-  
-    useEffect(() => {
-      if (dropDownOpen && dropdownRef !== null) {
-        dropdownRef.current.focus();
-      }
-    }, [dropDownOpen]);
-  
-  
-    return (
-      <div className="dropdownContainer">
-        <div className="dropdownButton"
-          onClick={updateDropDown}
-          ref={dropdownRef}
-        >
-          {currentCurrency}
-        </div>
-          {dropDownOpen && (
-            <div className="dropdownList"
-              onBlur={closeDropDown}
-              ref={dropdownRef}
-            >
-              <CurrencyDropdownList handleDropSelection={handleDropSelection} />
-            </div>
-          )}
-      </div>
-    );
+export const Dropdown = ({
+  handleSelection,
+  currentCurrency,
+}: DropdownProps) => {
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+
+  const closeDropDown = () => {
+    setDropDownOpen(false);
   };
+
+  const updateDropDown = () => {
+    setDropDownOpen(!dropDownOpen);
+  };
+
+  const handleDropSelection = (selection: string) => {
+    handleSelection(selection);
+    closeDropDown();
+  };
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (dropDownOpen && dropdownRef.current !== null) {
+      dropdownRef.current.focus();
+    }
+  }, [dropDownOpen]);
+
+  return (
+    <div className="dropdownContainer">
+      <div
+        className="dropdownButton"
+        onClick={updateDropDown}
+        ref={dropdownRef}
+      >
+        {currentCurrency}
+      </div>
+      {dropDownOpen && (
+        <div className="dropdownList" onBlur={closeDropDown} ref={dropdownRef}>
+          <CurrencyDropdownList handleDropSelection={handleDropSelection} />
+        </div>
+      )}
+    </div>
+  );
+};
