5805170d93f27f8e494e52a507002c7b47853178
Kyle Starrett <akstarre@gmail.com>
2023-10-06 15:01:03 -0400

    imports switched around a little, not saving this commit, just experimenting with import organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import React, { useEffect } from "react";
-import tw from "tailwind-styled-components";
 import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/app/GlobalRedux/store";
-import { useAppSelector } from "./GlobalRedux/store";
+import tw from "tailwind-styled-components";
 import { Navbar } from "@/components/Navbar";
+import { AppDispatch, RootState } from "@/app/GlobalRedux/store";
+import { fetchCoins } from "@/app/GlobalRedux/Features/MarketTable";
+import { changeChart } from "@/app/GlobalRedux/Features/CurrencySlice";
 import { CoinCharts } from "@/components/CoinCharts";
 import MarketTable from "../components/MarketTable/index";
 import { CoinSelectorCarousel } from "../components/CoinSelectorCarousel/index";
-import { fetchCoins } from "@/app/GlobalRedux/Features/MarketTable";
-import { changeChart } from "@/app/GlobalRedux/Features/CurrencySlice";
+import { useAppSelector } from "./GlobalRedux/store";
 import { CoinsData } from "./FakeData/CoinsData";
 import { GlobalData } from "./FakeData/GlobalData";
 import { BitcoinDailyData } from "@/app/FakeData/BitcoinDailyData";
