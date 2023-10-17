9678de911687fa67529f64b68c6b223904b0eead
Kyle Starrett <akstarre@gmail.com>
2023-10-17 11:32:12 -0400

    altered formatNumber and formattChartNumber to handle larger negative numbers.

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -10,13 +10,25 @@ export const formatNumber = (num: number) => {
     const finalNum = (num / 1000000000000).toFixed(2);
     return `${finalNum}T`;
   }
-  if (num > 1000000) {
+  if (num > 1000000000) {
     const finalNum = (num / 1000000000).toFixed(2);
     return `${finalNum}B`;
   }
   if (num > 1000000) {
     const finalNum = (num / 1000000).toFixed(2);
     return `${finalNum}M`;
+  }
+  if (num < -1000000) {
+    const finalNum = (num / 1000000).toFixed(2);
+    return `${finalNum}M`;
+  }
+  if (num < -1000000000) {
+    const finalNum = (num / 1000000000).toFixed(2);
+    return `${finalNum}B`;
+  }
+  if (num < -1000000000000) {
+    const finalNum = (num / 1000000000000).toFixed(2);
+    return `${finalNum}T`;
   }
   return num.toFixed(2);
 };
@@ -37,6 +49,18 @@ export const formatChartNumber = (num: number) => {
   if (num > 1000000) {
     const finalNum = (num / 1000000).toFixed(3);
     return `${finalNum} mln`;
+  }
+  if (num < -1000000) {
+    const finalNum = (num / 1000000).toFixed(3);
+    return `${finalNum} mln`;
+  }
+  if (num < -1000000000) {
+    const finalNum = (num / 1000000000).toFixed(3);
+    return `${finalNum} bln`;
+  }
+  if (num < -1000000000000) {
+    const finalNum = (num / 1000000000000).toFixed(3);
+    return `${finalNum} tln`;
   }
   return num.toFixed(3);
 };
